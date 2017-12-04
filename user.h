0c6e81c7093205697a14186889fb8d5ad199aa80
dltkdals224 <dltkdals224@users.noreply.github.com>
2017-12-04 22:18:06 +0900

    user.h - 12/4 수정본
    채은이 ?_to_?랑 정원이,채은이 형맞추는거 다 해놨음 일단.
    함수원형 아직 작성안됨.

--- user.h
+++ user.h
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <signal.h>
 
 #define MAN 0
 #define FEMALE 1
@@ -11,48 +12,78 @@
 #ifndef _USER_H_
 #define _USER_H_ // _USER_H_가 정의되어 있지 않을 때 정의되도록.
 
-struct stack{
+
+typedef struct linked_list{
   char *str; //string(문장) 저장
-  struct stack *next; //링크드 리스트를 위해 존재.
+  struct linked_list *next; //링크드 리스트를 위해 존재.
+}stack; //정원이가 그냥 stack , 채은이가 struct linked_list
+
+//파일이 아니고 구조체.
+struct director_list{
+  int serial_number;
+  struct linked_list *name;
+  struct linked_list *sex;
+  struct linked_list *birth;
+  struct d_title_to_title *best_movies;
+  struct director_list *next;
 };
 
-struct director_struct{
-  int Serial_number;
-  struct stack *name;
-  struct stack *sex;
-  struct stack *birth;
-  struct stack *best_movies;
-  struct stack *next;
+//파일이 아니고 구조체.
+struct actor_list{
+  int serial_numer;
+  struct linked_list *name;
+  struct linked_list *sex;
+  struct linked_list *birth;
+  struct a_title_to_title *best_movies;
+  struct actor_list *next;
 };
 
-struct actor_struct{
-  int Serial_numer;
-  struct stack *name;
-  struct stack *sex;
-  struct stack *birth;
-  struct stack *best_movies;
-  struct stack *next;
+//파일이 아니고 구조체.
+struct movie_list{
+  int serial_numer;
+  struct linked_list *title;
+  struct linked_list *genre;
+  struct movie_to_director *director;
+  int year;
+  int time;
+  struct movie_to_actor *actors;
+  struct movie_list *next;
 };
 
-struct movie_struct{
-  int Serial_numer;
-  struct stack *title;
-  struct stack *genre;
-  struct stack *director;
-  struct stack *year;
-  struct stack *time;
-  struct stack *actors;
-  struct stack *next;
+struct movie_to_director {
+	char *str;
+	struct director_list *pair;
+	struct movie_to_director *next;
 };
 
-//단순화 시킨 것.
-typedef struct director_struct director;
-typedef struct actor_struct actor;
-typedef struct movie_struct movie;
+struct movie_to_actor {
+	char *str;
+	struct actor_list *pair;
+	struct movie_to_actor *next;
+};
+
+struct a_title_to_title {
+	char *str;
+	struct movie_list *title;
+	struct d_title_to_title *pair;
+	struct a_title_to_title *next;
+};
+
+struct d_title_to_title {
+	char *str;
+	struct movie_list *title;
+	struct a_title_to_title *pair;
+	struct d_title_to_title *next;
+};
+
+//단순화 시킨 것.(정원이가 쓰는데 혹시 몰라서 <- 필요하대요)
+typedef struct director_list director;
+typedef struct actor_list actor;
+typedef struct movie_list movie;
 
 //링크드 리스트 사용할때 쓸 것.
-typedef struct stack tmp;
-typedef tmp *LINK;
+typedef struct linked_list linked_list; // 혹시몰라서.
+typedef stack *LINK; // 왜 써 놨더라?
 
 #endif // _USER_H_
 
